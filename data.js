94de4fbe32836d53812b2c49c2e2e5a67fb2d744
Lee Cookson <lee.cookson@dowjones.com>
2013-05-19 09:39:53 -0400

    added update method (not tested), addd ensure Index for unitReportTime and id

--- data.js
+++ data.js
@@ -100,6 +100,10 @@ Data.prototype.loadDB = function (type, options, cb) {
         console.error('error', err);
         return cb(new Error('db error'));
       }
+      collection.ensureIndex('reportUnixTime', true, function (err, replies) {});
+      collection.ensureIndex('id', true, function (err, replies) {});
+
+
       collection.find(options.query).limit(options.limit).sort('_id').toArray(function (err, data) {
         if (!data) return;
         if (self.options.logLevel) console.log('    loaded', data ? data.length : -1, type);
@@ -193,6 +197,25 @@ Data.prototype.saveDB = function (type, cb) {
       }
       );
     });
+  });
+};
+
+Data.prototype.update = function (type, criteria, values, options, cb) {
+  this.db.collection(type + 'Data', function (err, collection) {
+
+    if (err) {
+      console.error('error', err);
+      return cb(new Error('db error'));
+    }
+    collection.update(
+      criteria,
+      values,
+      options,
+      function (err, docs) {
+        if (err) return cb(err);
+        cb(null, docs[0]);
+      }
+    );
   });
 };
 
